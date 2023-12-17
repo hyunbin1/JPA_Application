96ef67451ca8c7f31b6faee07d67ccbc84f01961
hyunbin1 <kimgusqls1@gmail.com>
2023-12-17 12:43:00 +0900

    [add] 상품 수정 controller

--- src/main/java/jpabook/jpashop/controller/ItemController.java
+++ src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -8,6 +8,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -42,5 +44,36 @@ public class ItemController {
         List<Items> items = itemService.findAllItem();
         model.addAttribute("items", items);
         return "items/itemList";
+    }
+
+    // 상품 수정
+    @GetMapping("items/{itemId}/edit")
+    public String updateItemForm(@PathVariable("itemId") Long itemId, Model model) {
+        Book item = (Book) itemService.findItem(itemId);
+
+        BookForm bookForm = new BookForm();
+        bookForm.setBookId(item.getItemId());
+        bookForm.setName(item.getItemName());
+        bookForm.setBookPrice(item.getPrice());
+        bookForm.setStockQuantity(item.getStockQuantity());
+        bookForm.setAuthor(item.getAuthor());
+        bookForm.setIsbn(item.getIsbn());
+
+        model.addAttribute("form", bookForm);
+        return "items/updatedItemForm";
+    }
+
+    @PostMapping("items/{itemId}/edit")
+    public String updateItem(@PathVariable String itemId, @ModelAttribute("bookForm") BookForm form) {
+        Book book = new Book();
+        book.setItemId(form.getBookId());
+        book.setItemName(form.getName());
+        book.setPrice(form.getBookPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+
+        itemService.saveItem(book);
+        return "redirect:/items";
     }
 }
